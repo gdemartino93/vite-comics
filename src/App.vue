ced2b4fcb84093c95a3030e9015ac2ee1d4c2f53
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-12-12 21:30:31 +0100

    header

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <script >
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue'
+export default{
+  name : "App",
+  components :{ AppHeader} 
+}
 </script>
 
 <template>
-  <div>
-    
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <AppHeader />
 </template>
 
 <style lang="scss" >
@@ -27,5 +22,6 @@ h6{
   color: $primary;
   background: $secondary;
 }
+
 
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,101 @@
+<script >
+export default{
+  data(){
+    return{
+      links : [
+        {
+          text : "Characters",
+          url : "#",
+          current : false
+        },
+        {
+          text : "Comics",
+          url : "#",
+          current : false
+        },
+        {
+          text : "Movies",
+          url : "#",
+          current : false
+        },
+        {
+          text : "TV",
+          url : "#",
+          current : false
+        },
+        {
+          text : "Games",
+          url : "#",
+          current : false
+        },
+        {
+          text : "Collectibles",
+          url : "#",
+          current : true
+        },
+        {
+          text : "Videos",
+          url : "#",
+          current : false
+        },
+        {
+          text : "Fans",
+          url : "#",
+          current : false
+        },
+        {
+          text : "News",
+          url : "#",
+          current : false
+        },
+        {
+          text : "Shop",
+          url : "#",
+          current : false
+        },
+
+      ]
+    }
+  }
+}
+
+</script>
+
+<template>
+  <header>
+    <a href="">
+      <img src="../../public/dc-logo.png" alt="">
+    </a>
+    <ul>
+      <li v-for="(link, index) in links" :key="index">
+        <a :href="link.url" :class="{active: link.current}">
+          {{(link.text).toUpperCase()}}
+        </a>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+
+header{
+  max-width: 1300px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 auto;
+
+ ul{
+  display: flex;
+  list-style: none;
+  gap: 20px;
+    a{
+    text-decoration: none;
+    color: black;
+    font-weight: bold;
+    }
+ }
+}
+
+</style>
