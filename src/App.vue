83da30e366e1de5d19f2ee570d6e45532cac2913
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-12-13 00:12:07 +0100

    social contact

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,10 @@ import AppHeader from './components/AppHeader.vue'
 import AppMain from './components/AppMain.vue'
 import BluSection from './components/BluSection.vue'
 import FooterLink from './components/FooterLink.vue'
+import Social from './components/Social.vue'
 export default{
   name : "App",
-  components :{ AppHeader, AppMain, BluSection, FooterLink} 
+  components :{ AppHeader, AppMain, BluSection, FooterLink, Social} 
 }
 </script>
 
@@ -14,6 +15,7 @@ export default{
   <AppMain />
   <BluSection />
   <FooterLink />
+  <Social />
 </template>
 
 <style lang="scss" >
---
+++ src/components/Social.vue
@@ -0,0 +1,58 @@
+<script >
+export default{
+  data(){
+    return{
+
+    }
+  }
+}
+
+</script>
+
+<template>
+<section class="social">
+    <div class="container">
+
+        <a href="" class="btn">SIGN-UP NOW!</a>
+        <div class="contact"> 
+            <span>FOLLOW US</span>
+            <img src="../../public/footer-facebook.png" alt="">
+            <img src="../../public/footer-twitter.png" alt="">
+            <img src="../../public/footer-youtube.png" alt="">
+            <img src="../../public/footer-pinterest.png" alt="">
+            <img src="../../public/footer-periscope.png" alt="">
+        
+        </div>
+    </div>
+</section>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+.social{
+    background: #333333;
+
+
+    .container{
+        max-width: 1300px;
+        margin:  0 auto;
+        display: flex;
+        align-items: center;
+        height: 15vh;
+        justify-content: space-between;
+        .btn{
+            padding: 10px 10px;
+            border: 2px solid $bg-blue;
+            color: white;
+        }
+        .contact{
+            color: $bg-blue;
+            font-weight: bold;
+            display: flex;
+            gap:20px;
+            align-items: center;
+        }
+
+    }
+}
+</style>
